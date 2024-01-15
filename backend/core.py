0e92a835635e81b99b9aa81e089a0e0a7d1854b9
rislaazeez <rizlaazeez@icloud.com>
2024-01-14 22:27:19 -0800

    first commit

---
+++ backend/core.py
@@ -0,0 +1,33 @@
+import os
+from typing import Any, List, Dict
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import Pinecone
+import pinecone
+
+from consts import INDEX_NAME
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(
+        index_name=INDEX_NAME, embedding=embeddings
+    )
+    chat = ChatOpenAI(verbose=True, temperature=0)
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+
+    return qa({"question": query, "chat_history": chat_history})
+
+
+if __name__ == "__main__":
+    print(run_llm(query="What is LangChain?"))
